215f208d2e4afc32b25e2bc0f1080e30b70e088f
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-20 09:02:43 +0200

    views without logic added

--- Online_Library_remake_exam/my_web/urls.py
+++ Online_Library_remake_exam/my_web/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from Online_Library_remake_exam.my_web.views import index, add_book, edit_book, details_book, delete_book, \
+    details_profile, create_profile, edit_profile, delete_profile
+
 urlpatterns = (
     path('', include([
         path('', index, name='index'),
@@ -14,7 +17,7 @@ urlpatterns = (
         path('create/', create_profile, name='create-profile'),
         path('edit/', edit_profile, name='edit-profile'),
         path('delete/', delete_profile, name='delete-profile'),
-    ]))
+    ])),
 )
 
 """
--- Online_Library_remake_exam/my_web/views.py
+++ Online_Library_remake_exam/my_web/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    profile = None
+
+    if not profile:
+        return redirect('create-profile')
+
+    return render(request, 'common/home-with-profile.html')
+
+
+# Profile views:
+def create_profile(request):
+    return render(request, 'profile/create-profile.html')
+
+
+def details_profile(request):
+    return render(request, 'profile/details-profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile/edit-profile.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile/delete-profile.html')
+
+
+# Books views:
+def add_book(request):
+    return render(request, 'book/add-book.html')
+
+
+def edit_book(request, pk):
+    return render(request, 'book/edit-book.html')
+
+
+def details_book(request, pk):
+    return render(request, 'book/book-details.html')
+
+
+def delete_book(request, pk):
+    pass
