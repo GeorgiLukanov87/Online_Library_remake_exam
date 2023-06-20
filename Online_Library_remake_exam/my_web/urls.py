f131672555b19a24fc0925da0f90577a5d25c810
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-20 08:54:31 +0200

    adding urls

--- Online_Library_remake_exam/my_web/urls.py
+++ Online_Library_remake_exam/my_web/urls.py
@@ -1,3 +1,37 @@
-urlpatterns = (
+from django.urls import path, include
 
+urlpatterns = (
+    path('', include([
+        path('', index, name='index'),
+        path('add/', add_book, name='add-book'),
+        path('edit/<int:pk>/', edit_book, name='edit-book'),
+        path('details/<int:pk>/', details_book, name='details-book'),
+        path('delete/<int:pk>/', delete_book, name='delete-book'),
+    ])),
+
+    path('profile/', include([
+        path('', details_profile, name='details-profile'),
+        path('create/', create_profile, name='create-profile'),
+        path('edit/', edit_profile, name='edit-profile'),
+        path('delete/', delete_profile, name='delete-profile'),
+    ]))
 )
+
+"""
+
+http://localhost:8000/ - home page
+
+http://localhost:8000/add/ - add book page
+http://localhost:8000/edit/1 - edit book page
+http://localhost:8000/details/1 - book details page
+additional
+http://localhost:8000/delete/1 - book delete page
+
+
+http://localhost:8000/profile/ -  details profile page
+http://localhost:8000/profile/edit/ - edit profile page
+http://localhost:8000/profile/delete/ - delete profile page
+additional
+http://localhost:8000/profile/create/ - create profile page
+
+"""
