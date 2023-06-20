af516dd6b9b385bf935268a66cde5762d327f128
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-20 10:39:48 +0200

    .

--- Online_Library_remake_exam/my_web/models.py
+++ Online_Library_remake_exam/my_web/models.py
@@ -11,14 +11,14 @@ class ProfileModel(models.Model):
 # Book model
 class BookModel(models.Model):
     CHOICES = (
-        ('1.Fiction', 'Fiction'),
-        ('2.Novel', 'Novel'),
-        ('3.Crime', 'Crime'),
-        ('4.Fantasy', 'Fantasy'),
-        ('5.Roman', 'Roman'),
-        ('6.Study', 'Study'),
-        ('7.Modern', 'Modern'),
-        ('8.Horror', 'Horror'),
+        ('Fiction', '1.Fiction'),
+        ('Novel', '2.Novel'),
+        ('Crime', '3.Crime'),
+        ('Fantasy', '4.Fantasy'),
+        ('Roman', '5.Roman'),
+        ('Study', '6.Study'),
+        ('Modern', '7.Modern'),
+        ('Horror', '8.Horror'),
     )
 
     title = models.CharField(max_length=30)
